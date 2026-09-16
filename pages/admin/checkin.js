14873813a9afc9718be4ba53987c5e8ea63e974c
luxchristoph42 <luxchristoph96@gmail.com>
2026-09-16 09:45:54 +0200

    Update checkin.js

--- pages/admin/checkin.js
+++ pages/admin/checkin.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import { supabase } from '../../lib/supabase';
 import Link from 'next/link';
-import AdminScheduleSettings from '../../components/AdminScheduleSettings';
-import AdminPlayerTable from '../../components/AdminPlayerTable';
-import AdminMatchList from '../../components/AdminMatchList';
+import AdminScheduleSettings from './AdminScheduleSettings';
+import AdminPlayerTable from './AdminPlayerTable';
+import AdminMatchList from './AdminMatchList';
 
 export default function AdminCheckin() {
   const [password, setPassword] = useState('');
